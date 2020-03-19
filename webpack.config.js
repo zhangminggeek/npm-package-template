44ab3795124d6fd460d6189f5d4cb5d0f6204fca
zhangming <qianzm2490@vip.qq.com>
2020-03-19 18:09:36 +0800

    chore: add HotModuleReplacementPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
@@ -6,7 +7,7 @@ module.exports = {
   entry: path.join(__dirname, './example/src/app.jsx'),
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash:8].js'
+    filename: '[name].[hash:8].js'
   },
   module: {
     rules: [
@@ -46,7 +47,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, './example/src/index.html'),
       filename: './index.html'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   resolve: {
     extensions: ['.js', '.jsx']
